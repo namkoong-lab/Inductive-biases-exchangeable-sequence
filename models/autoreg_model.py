13ea7e02ee34d22ccd04bb09012a8b5ccf654ccf
Leon Li <al4263@researchgpu07.gsb.columbia.edu>
2024-11-07 10:26:05 -0500

    data

--- models/autoreg_model.py
+++ models/autoreg_model.py
@@ -3,46 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.normal import Normal
 
-from models.modules import build_mlp
+from models.modules import build_mlp, RiemannDistribution
 from collections import namedtuple
 
-
-class RiemannDistribution(nn.Module):
-    def __init__(self, borders, accelerator = None):
-        super().__init__()
-        assert len(borders.shape) == 1, "Borders should be a 1D tensor."
-        self.accelerator = accelerator
-        self.register_buffer('borders', borders)
-        self.register_buffer('bucket_widths', borders[1:] - borders[:-1])
-        self.num_buckets = len(borders) - 1
-
-    def map_to_bucket_idx(self, y):
-        # Map each target value y to its corresponding bucket index
-        target_sample = torch.searchsorted(self.borders, y, right=False) - 1
-        target_sample = target_sample.clamp(0, self.num_buckets - 1)
-        return target_sample
-
-    def forward(self, logits, y):
-        # logits: (batch_size, seq_len, num_buckets)
-        # y: (batch_size, seq_len)
-        target_sample = self.map_to_bucket_idx(y)  # (batch_size, seq_len)
-        bucket_log_probs = F.log_softmax(logits, dim=-1)  # (batch_size, seq_len, num_buckets)
-        log_bucket_widths = torch.log(self.bucket_widths)  # (num_buckets,)
-        scaled_log_probs = bucket_log_probs - log_bucket_widths  # Broadcasting over num_buckets
-        # Gather the log probabilities for the target buckets
-        log_probs = scaled_log_probs.gather(-1, target_sample.unsqueeze(-1)).squeeze(-1)  # (batch_size, seq_len)
-        # Compute the negative log-likelihood loss
-        loss = -log_probs.mean()
-        return log_probs, loss
-
-    def sample(self, logits):
-
-        bucket_indices = torch.distributions.Categorical(logits=logits).sample()  # (batch_size, seq_len)
-        # Sample uniformly within the bucket
-        bucket_lefts = self.borders[:-1][bucket_indices]  # (batch_size, seq_len)
-        bucket_rights = self.borders[1:][bucket_indices]  # (batch_size, seq_len)
-        samples = bucket_lefts + (bucket_rights - bucket_lefts) * torch.rand_like(bucket_lefts)
-        return samples
 
 class Autoreg_Model(nn.Module):
     def __init__(
